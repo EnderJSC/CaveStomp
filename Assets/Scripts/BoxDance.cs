9333a77a4979024ef59b95b899a160f388948eaf
Jeff Cotton <EnderJSC@hotmail.com>
2012-04-28 16:39:17 -0700

    Changed the Box dance sript to slowly rotate a box to a random target location

--- Assets/Scripts/BoxDance.cs
+++ Assets/Scripts/BoxDance.cs
@@ -4,15 +4,36 @@ using System.Collections;
 public class BoxDance
 	: MonoBehaviour
 {
-
+	private float 		fSlerpValue 		= 0.0f;
+	private Quaternion 	quatInitialRotation	= Quaternion.identity;
+	private Quaternion 	quatTargetRotation 	= Quaternion.identity;
+	
 	// Use this for initialization
 	void Start ()
 	{
+		InitializeNewTargetRotation();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		transform.rotation = Random.rotation;
+		fSlerpValue += Time.deltaTime / 10.0f;
+		if ( fSlerpValue > 1.0f )
+		{
+			fSlerpValue = 1.0f;
+			transform.rotation = quatTargetRotation;
+			InitializeNewTargetRotation();
+		}
+		else
+		{
+			transform.rotation = Quaternion.Lerp( quatInitialRotation, quatTargetRotation, fSlerpValue );
+		}
+	}
+	
+	public void InitializeNewTargetRotation()
+	{
+		fSlerpValue = 0.0f;
+		quatInitialRotation = transform.rotation;
+		quatTargetRotation = Random.rotation;
 	}
 }
